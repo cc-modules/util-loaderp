6e73719a99fbcc65ea3f16aabfcf457ece45e7e4
wangweihua <aztack@163.com>
2018-07-06 14:14:09 +0800

    bigbang

---
+++ util-loaderp.js
@@ -0,0 +1,48 @@
+/**
+ * Extends cc with loaderp (promisified loader)
+ */
+if (cc && cc.loader) {
+  const loader = cc.loader;
+  const loaderp = cc.loaderp = {};
+
+  var promisify = function (method) {
+    return function (...args) {
+      return new Promise((rs, rj) => {
+        args.push((err, data) => {
+          if (err) {
+            rj(err);
+          } else {
+            rs(data);
+          }
+        });
+        loader[method].apply(loader, args)
+      })
+    };
+  };
+
+  /**
+   * Promisified cc.loader.loadRes
+   */
+  loaderp.loadRes = promisify('loadRes');
+
+  /**
+   * Promisified cc.loader.load
+   */
+  loaderp.load = promisify('load')
+
+  /**
+   * Load multiple resource
+   * const resources = [
+   *  ['atlas', cc.SpriteAtlas],
+   *  ['json']
+   * ];
+   * cc.loaderp.loadResAll(resources).then(([atlas, json]) => {
+   *   this.atlas = atlas;
+   *   this.json = json;
+   * })
+   */
+  loaderp.loadResAll = function (paramArray) {
+    const promises = paramArray.map(args => cc.loaderp.loadRes.apply(cc.loaderp, args));
+    return Promise.all(promises);
+  };
+}
